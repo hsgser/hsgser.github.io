b713b2ec4e1c55ff59933b94aac7ebcc2f51afc0
hsgser <31280959+hsgser@users.noreply.github.com>
2025-05-17 04:53:54 +0000

    Deploying to gh-pages from @ hsgser/hsgser.github.io@af673b8240de3f031472f4412bf654f16118df7b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,6 +92,9 @@ ninja.data = [{
           section: "News",},{id: "news-our-paper-synthetic-text-generation-for-training-large-language-models-via-gradient-matching-is-accepted-to-icml-2025",
           title: 'Our paper Synthetic Text Generation for Training Large Language Models via Gradient Matching...',
           description: "",
+          section: "News",},{id: "news-our-paper-a-black-box-approach-to-uncertainty-quantification-in-llms-overcoming-the-limits-of-semantic-entropy-is-accepted-to-acl-findings-2025",
+          title: 'Our paper A Black‑Box Approach to Uncertainty Quantification in LLMs: Overcoming the Limits...',
+          description: "",
           section: "News",},{
         id: 'social-email',
         title: 'email',
